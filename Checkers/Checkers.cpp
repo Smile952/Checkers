73f283fd241595df12ae4722997bb2d983abbd79
Smile952 <deniska1234deins@gmail.com>
2024-05-18 12:23:30 +0700

    Added bot play

--- Checkers/Checkers.cpp
+++ Checkers/Checkers.cpp
@@ -5,19 +5,21 @@ using namespace std;
 
 const int w = 1;
 const int b = 2;
-const int W = 3;
-const int B = 4;
 
 int amount_w = 12;
 int amount_b = 12;
-
-int coord_x[12];
-int coord_y[12];
 
 int map[8][8];
 
 int player = 1;
 bool isEaten = false;
+
+int botStepsFrom_x[12];
+int botStepsFrom_y[12];
+int botStepsTo_x[12];
+int botStepsTo_y[12];
+
+int botAmountSteps = 0;
 
 void createMap() {
     for (int i = 0; i < 8; i++) {
@@ -175,27 +177,82 @@ void outputMap() {
     }
 }
 
+void canBotStep(int col, int row) {
+    if (inBorder(col + 1, row + 1) && map[col + 1][row + 1] == 0) {
+        botStepsFrom_x[botAmountSteps] = col;
+        botStepsFrom_y[botAmountSteps] = row;
+        botStepsTo_x[botAmountSteps] = col + 1;
+        botStepsTo_y[botAmountSteps] = row + 1;
+        botAmountSteps++;
+
+    }
+    
+    if (inBorder(col + 1, row + 1) && map[col + 1][row + 1] == 0) {
+        botStepsFrom_x[botAmountSteps] = col;
+        botStepsFrom_y[botAmountSteps] = row;
+        botStepsTo_x[botAmountSteps] = col + 1;
+        botStepsTo_y[botAmountSteps] = row + 1;
+        botAmountSteps++;
+    }
+
+}
+
+void createBotListSteps() {
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8;  j++) {
+            if (map[i][j] == 2) {
+                canBotStep(i, j);
+            }
+        }
+    }
+}
+
+int getRandomBotStep() {
+    return rand() % botAmountSteps;
+}
+
+void botStep(int index) {
+    map[botStepsFrom_x[index]][botStepsFrom_y[index]] = 0;
+    map[botStepsTo_x[index]][botStepsTo_y[index]] = 2;
+    cout << "Player 2 step from: " << botStepsFrom_x[index] << " " << botStepsFrom_y[index] << endl;
+    cout << "Player 2 step to: " << botStepsTo_x[index] << " " << botStepsTo_y[index] << endl;
+}
 
 int main()
 {
+    int botStepIndex = 0;
     createMap();
     int col, row, col_past, row_past, step_type, temp_i, temp_j = 0;
     while (amount_b > 0 || amount_w > 0) {
-        outputMap();
-        cin >> col >> row;
-        while (!isCorrectCoord(col, row)) {
-            cout << "Uncorrect coord for figure" << "\n";
+        
+        if (player == 1) {
+            outputMap();
             cin >> col >> row;
-        }
-        col_past = col; 
-        row_past = row;
-        cin >> step_type;
-        while (!step(step_type, col, row)) {
+            while (!isCorrectCoord(col, row)) {
+                cout << "Uncorrect coord for figure" << "\n";
+                cin >> col >> row;
+            }
+            col_past = col;
+            row_past = row;
             cin >> step_type;
+            while (!step(step_type, col, row)) {
+                cin >> step_type;
+            }
+            map[col][row] = player;
+            map[col_past][row_past] = 0;
+            cout << "Player 1 step from : " << col_past << " " << row_past << endl;
+            cout << "Player 2 step to: " << col << " " << row<< endl;
         }
-        map[col][row] = player;
-        map[col_past][row_past] = 0;
-        switchPlayer();
+        else {
+            createBotListSteps();
+            botStepIndex = getRandomBotStep();
+            botStep(botStepIndex);
+            botAmountSteps = 0;
+            outputMap();
+        }
         isEaten = false;
+        switchPlayer();
+        cout << endl;
+        cout << endl;
     }
 }
